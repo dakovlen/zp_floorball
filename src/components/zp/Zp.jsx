4d6676ed2d1ff761b5852351229b1d0764e2e8cb
dakovlen <dakovlen@gmail.com>
2022-01-05 12:51:14 +0200

    Update: added base file in project

---
+++ src/components/zp/Zp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './zp.css';
+
+const Zp = () => {
+  return(
+    <div>
+      Zp
+    </div>
+  )
+}
+
+export default Zp
---
+++ src/containers/whatZp/Whatzp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './whatzp.css';
+
+const Whatzp = () => {
+  return(
+    <div>
+      Whatzp
+    </div>
+  )
+}
+
+export default Whatzp
